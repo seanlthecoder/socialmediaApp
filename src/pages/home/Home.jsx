8a3bfabffc586a4222c8e31d9df1584a358ac7fa
Sean Lester Durham <sean.durham1982@gmail.com>
2023-05-20 22:43:16 -0500

    added home pages

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <div className="profileSide">Profile</div>
+            <div className="postSide">Posts</div>
+            <div className="RightSide">RightSide</div>
+
+
+        </div>
+    )
+}
+
+export default Home

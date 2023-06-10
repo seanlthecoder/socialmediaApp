e339de81e0149feaaca62e59602f7da914715dc8
Sean Lester Durham <sean.durham1982@gmail.com>
2023-06-09 19:05:48 -0500

    updated logo

--- src/components/profileside/LogoSearch/LogoSearch.jsx
+++ src/components/profileside/LogoSearch/LogoSearch.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
+import LOGO4 from '../../../img/LOGO4.png'
+
 const LogoSearch = () => {
     return (
         <div className="LogoSearch">
-            LogoSearch
-
+            <img src={LOGO4} alt="" />
         </div>
-    )
+    );
 }
 
-export default LogoSearch
+export default LogoSearch;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import logo4 from '../..img/logo4.png'
+// <img src="" alt="" />
+// const LogoSearch = () => {
+//     return (
+//         <div className="LogoSearch">
+//             <img src={logo4} alt="" />
+
+//         </div>
+//     )
+// }
+
+// export default LogoSearch

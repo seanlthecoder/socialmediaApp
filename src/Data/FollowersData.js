7061f4dcddb48616b4ccf345a190c3c2e03526b3
Sean Durham <sdurham@tmscorp.com>
2023-06-29 16:24:08 -0500

    updated followers page

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,19 @@
+import img1 from '../img/IMG_1403.jpg';
+import img2 from '../img/IMG_2012.jpg';
+import img3 from '../img/IMG_4276.jpg';
+import img4 from '../img/IMG_5084.jpg';
+import img5 from '../img/IMG_6267.jpg';
+
+export const Followers = [
+    { name: "Oldest Brother", username: "OldestAustin", img: img1},
+
+    { name: "Aidan", username: "KingAidan", img: img2},
+
+    { name: "Mom", username: "Ma", img: img3},
+
+    { name: "Pop", username: "Dad", img: img4},
+
+    { name: "Family", username: "MyFam", img: img5},
+
+
+];
--- src/components/FollowersCard/FollowersCard.jsx
+++ src/components/FollowersCard/FollowersCard.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import './FollowersCard.css'
+import React from "react";
+import "./FollowersCard.css";
+import { Followers } from "../../Data/FollowersData";
 
 const FollowersCard = () => {
+  return (
+    <div className="FollowerCard">
+      <h3>Who is Following You</h3>
 
-    return (
-        <div className="FollowerCard">
-            <h3>Who is Following You</h3>
+      {Followers.map((follower, id) => {
+        return (
+          <div className="follower">
+            <div>
+              <img src={follower.img} alt="" className="followerImg" />
+              <div className="name">
+                <span>{follower.name}</span>
+                <span>@{follower.username}</span>
+              </div>
+            </div>
+            <button className='button'>Follow</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-        </div>
-
-
-    )
-
-
-}
-
-export default FollowersCard
+export default FollowersCard;
